988d630700346f8d5cec2e6a63d72ab8fdd4c4c6
smeyfroi <steve.meyfroidt@gmail.com>
2024-10-06 19:31:30 +0100

    Parameterise sample validity check

--- src/Processor.cpp
+++ src/Processor.cpp
@@ -11,8 +11,12 @@ void Processor::update() {
   audioAnalysisClientPtr->update();
 }
 
-bool Processor::isDataValid() {
-  return audioAnalysisClientPtr->getScalarValue(ofxAudioAnalysisClient::AnalysisScalar::rootMeanSquare) > 100 && audioAnalysisClientPtr->getScalarValue(ofxAudioAnalysisClient::AnalysisScalar::pitch) < 5000 && audioAnalysisClientPtr->getScalarValue(ofxAudioAnalysisClient::AnalysisScalar::pitch) > 50;
+bool Processor::isDataValid(const std::vector<ValiditySpec>& sampleValiditySpecs) const {
+  for (const auto& spec : sampleValiditySpecs) {
+    if (spec.isLessThan && audioAnalysisClientPtr->getScalarValue(spec.scalar) > spec.value) return false;
+    if (!spec.isLessThan && audioAnalysisClientPtr->getScalarValue(spec.scalar) < spec.value) return false;
+  }
+  return true;
 }
 
 float Processor::getScalarValue(ofxAudioAnalysisClient::AnalysisScalar scalar) {
@@ -23,8 +27,7 @@ float* Processor::getScalarValuePtr(int scalar) {
   return audioAnalysisClientPtr->getScalarValuePtr(scalar);
 }
 
-float Processor::getNormalisedScalarValue(ofxAudioAnalysisClient::AnalysisScalar scalar, float minValue, float maxValue, bool isLinear) {
-  float value = getScalarValue(scalar);
+float Processor::normalisedValue(float value, float minValue, float maxValue, bool isLinear) const {
   if (!isLinear) {
     value = std::log(value);
     minValue = std::log(minValue);
@@ -33,6 +36,18 @@ float Processor::getNormalisedScalarValue(ofxAudioAnalysisClient::AnalysisScalar
   value = ofMap(value, minValue, maxValue, 0.0, 1.0);
   value = std::abs(value);
   return value - std::floor(value); // wrap around
+}
+
+float Processor::getNormalisedScalarValue(ofxAudioAnalysisClient::AnalysisScalar scalar, float minValue, float maxValue, bool isLinear) {
+  return normalisedValue(getScalarValue(scalar), minValue, maxValue, isLinear);
+}
+
+auto Processor::getNormalisedMfcc(float minValue, float maxValue) const {
+  std::vector<float> result;
+  for (float mfccValue : getMfcc()) {
+    result.push_back(normalisedValue(mfccValue, minValue, maxValue, false));
+  }
+  return result;
 }
 
 }
--- src/Processor.hpp
+++ src/Processor.hpp
@@ -5,17 +5,25 @@
 
 namespace ofxAudioData {
 
+struct ValiditySpec {
+  ofxAudioAnalysisClient::AnalysisScalar scalar;
+  bool isLessThan;
+  float value;
+};
+
 class Processor {
 public:
   Processor(std::shared_ptr<ofxAudioAnalysisClient::BaseClient> audioAnalysisClientPtr_);
   void update();
-  bool isDataValid();
+  bool isDataValid(const std::vector<ValiditySpec>& validitySpec) const;
   float getScalarValue(ofxAudioAnalysisClient::AnalysisScalar scalar);
   float* getScalarValuePtr(int scalar); // for Plots, via ofxHistoryPlot
   float getNormalisedScalarValue(ofxAudioAnalysisClient::AnalysisScalar scalar, float minValue, float maxValue, bool isLinear=true);
   inline auto& getMfcc() const { return audioAnalysisClientPtr->getMfcc(); };
+  auto getNormalisedMfcc(float minValue, float maxValue) const;
 
 private:
+  float normalisedValue(float value, float minValue, float maxValue, bool isLinear) const;
   std::shared_ptr<ofxAudioAnalysisClient::BaseClient> audioAnalysisClientPtr;
 };
 
